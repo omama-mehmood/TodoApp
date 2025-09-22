be8a8405b41bff13e9f05e29d8e3b255232ad32c
omama-mehmood <omama.mehmood@spursol.com>
2025-09-22 12:12:10 +0500

    Update ITodoService with nullable reference types and file-scoped namespace

--- Services/ITodoService.cs
+++ Services/ITodoService.cs
@@ -1,14 +1,12 @@
-using System.Collections.Generic;
 using TodoApp.Models;
 
-namespace TodoApp.Services
+namespace TodoApp.Services;
+
+public interface ITodoService
 {
-    public interface ITodoService
-    {
-        IEnumerable<TodoItem> GetAll();
-        TodoItem GetById(int id);
-        TodoItem Create(TodoItem todo);
-        TodoItem Update(int id, TodoItem todo);
-        bool Delete(int id);
-    }
+    IEnumerable<TodoItem> GetAll();
+    TodoItem? GetById(int id);
+    TodoItem Create(TodoItem todo);
+    TodoItem? Update(int id, TodoItem todo);
+    bool Delete(int id);
 }
